2df31b070f8e43c2a189fc1cd69abf5ef93c4f26
premprakashsingh <premsingh.singh386@gmail.com>
2016-04-09 16:56:49 +0000

    completed project

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
@@ -26,8 +26,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 routes(app, passport);
+*/
+var express=require("express");
+var app=express();
 
 var port = process.env.PORT || 8080;
+var host={};
+app.get("/",function(req,res){
+	//host["IP"]=req.ip;
+	/*res.send("it works...");*/
+	host.ipaddress=req.headers['x-forwarded-for'];//req.connection.remoteAddress;//req.ip.split(":")[3];
+	host.language=req.headers["accept-language"].split(",")[0];
+	host.software=req.headers['user-agent'].split(') ')[0].split(' (')[1];
+	res.send(host);
+	/*console.log('User-Agent: ' + req.headers['user-agent']);
+	res.send(JSON.stringify(req.get('user-agent')));*/
+})
+
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
